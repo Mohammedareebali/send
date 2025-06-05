3735576f37fee2d4ef4ced0704aa89f518adaafb
Mohammed Areeb Ali <88899913+Mohammedareebali@users.noreply.github.com>
2025-06-05 22:20:45 +0100

    Add unit tests for user service

---
+++ packages/user-service/jest.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/src'],
+  testMatch: ['**/__tests__/**/*.ts', '**/?(*.)+(spec|test).ts'],
+  transform: {
+    '^.+\\.ts$': 'ts-jest',
+  },
+  moduleNameMapper: {
+    '^@send/shared$': '<rootDir>/../shared/src',
+    '^@send/shared/(.*)$': '<rootDir>/../shared/src/$1',
+    '^@shared/(.*)$': '<rootDir>/../shared/src/$1',
+    '^@prisma/client$': '<rootDir>/../../test/prisma-client.ts'
+  },
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.test.json'
+    }
+  },
+  coverageDirectory: 'coverage',
+  collectCoverageFrom: [
+    'src/**/*.ts',
+    '!src/**/*.d.ts',
+    '!src/index.ts',
+    '!src/**/__tests__/**',
+  ],
+};
