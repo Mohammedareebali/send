1282a43892038abcdba4744aaf4af7270d273ffd
Mohammed Areeb Ali <88899913+Mohammedareebali@users.noreply.github.com>
2025-06-05 15:04:05 +0100

    Annotate transaction callback and extend Prisma stub

--- test/prisma-client.ts
+++ test/prisma-client.ts
@@ -1,4 +1,10 @@
 export class PrismaClient {
   [key: string]: any;
 }
+
 export const Prisma = {} as any;
+
+export namespace Prisma {
+  export type JsonObject = any;
+  export type TransactionClient = PrismaClient;
+}
