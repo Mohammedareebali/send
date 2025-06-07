e9533cf9be724524945cfb95992fe21868ef2358
areeb <mohammedareebali740@gmail.com>
2025-06-07 18:48:43 +0100

    Merge branch 'main' into codex/replace-console.log-  with-logger

--- packages/document-service/src/services/document.service.ts
+++ packages/document-service/src/services/document.service.ts
@@ -1,4 +1,4 @@
-import { PrismaClient, Prisma } from '@prisma/client';
+import { PrismaClient} from '@prisma/client';
 
 import { S3 } from 'aws-sdk';
 import { createWorker, Worker } from 'tesseract.js';
